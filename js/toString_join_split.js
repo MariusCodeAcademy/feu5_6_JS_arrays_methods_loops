4256f5f2f22c159e9c53f956855e13f4b92fa078
Marius CA <marius@example.com>
2023-02-13 12:29:14 +0200

    split join split

--- js/toString_join_split.js
+++ js/toString_join_split.js
@@ -12,3 +12,19 @@ document.getElementById('app').textContent = sent1;
 // Array.prototype.join(skirtukas)
 // sujungia masyva ir grazina stringa i iskvietimo vieta.
 // panaudoti join ir sujungti masyva ir irasyti i html sakini atskirta tarpeliais ir uzbaigti tasku.
+
+let stringFromArray = sent1.join(' ');
+console.log('stringFromArray ===', stringFromArray);
+
+document.getElementById('app').textContent = stringFromArray + '.!!!';
+
+// String.prototype.split(skirtukas)
+// atvirkscias veiksmas join()
+
+let strSent = 'Hello my name is James';
+let arrayFromString = strSent.split(' ');
+console.log('arrayFromString ===', arrayFromString);
+arrayFromString.reverse();
+console.log('arrayFromString ===', arrayFromString);
+let stringBackwards = arrayFromString.join(' ');
+console.log('stringBackwards ===', stringBackwards);
